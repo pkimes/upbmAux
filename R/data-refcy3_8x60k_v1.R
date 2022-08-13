31b7599e9e9d851d8029d1ed5597f56b68206e3e
Patrick Kimes <patrick.kimes@gmail.com>
2022-08-13 10:15:49 -0700

    Add all package contents

---
+++ R/data-refcy3_8x60k_v1.R
@@ -0,0 +1,10 @@
+#' Cy3 Reference Intensities for Standard PBM 8x60k (v1) Design
+#'
+#' Reference intensities for computed for a set of 90 Cy3 scans
+#' and was generated using \code{upbm::cy3GenerateRef}.
+#'
+#' @seealso \code{\link[upbm]{cy3GenerateRef}}
+#' @name refcy3_8x60k_v1
+#' @docType data
+#' @keywords data
+NULL
